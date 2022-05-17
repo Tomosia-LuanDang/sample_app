4002d9f03d4d54fa4afa1389968b61b649692a80
Tomosia-LuanDang <tt.luan.dang@tomosia.com>
2022-05-17 16:24:14 +0700

    Updating, showing, and deleting users

--- Gemfile
+++ Gemfile
@@ -6,7 +6,9 @@ ruby '2.7.0'
 gem 'bcrypt', '3.1.13'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'bootstrap-sass', '3.4.1'
+gem 'bootstrap-will_paginate', '1.0.0'
 gem 'config'
+gem 'faker', '2.1.2'
 gem 'rails-i18n'
 gem 'jbuilder', '~> 2.7'
 gem 'puma', '~> 5.0'
@@ -15,6 +17,7 @@ gem 'sass-rails', '>= 6'
 gem 'pg', '~> 1.3', '>= 1.3.5'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.0'
+gem 'will-paginate-i18n', '~> 0.1.15'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :show, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: Settings.size.digit_12)
+  end
+
   def new
-    logged_in? ? redirect_to(root_url) : @user = User.new
+    if logged_in?
+      redirect_to root_url
+    else
+      @user = User.new
+    end
   end
 
   def show
-    if logged_in?
-      @user = User.find(params[:id])
-    else
-      redirect_to login_path
-    end
+    @user = User.find(params[:id])
   end
 
   def create
@@ -22,9 +30,45 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = t "flash.update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = t "flash.user_del"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t "flash.plaese_login"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
